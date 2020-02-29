b3928d2242fa8bd2b7a757016f072ef8f0a6aac4
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-02-29 15:40:44 +0800

    null

---
+++ js/choice.js
@@ -0,0 +1,124 @@
+function Choice(items){
+	this.items=items;
+	this.init=function(){
+		this.box=
+			$("<div></div>")
+				.append(
+					$("<div></div>")
+						.css("position","absolute")
+						.css("left","0px")
+						.css("top","0px")
+						.css("height","100%")
+						.css("width","100%")
+						.css("background-color","#000")
+						.css("opacity","0.36")
+						.css("z-index","1")
+				)
+				.append(
+					$("<div></div>")
+						.css("position","absolute")
+						.css("left","0px")
+						.css("width","100%")
+						.css("bottom","0px")
+						.css("z-index","2")
+				)
+		;
+		for (var i in this.items){
+			this.box.find("div:eq(1)").append(
+				$("<div>"+this.items[i].name+"</div>")
+					.css("width","100%")
+					.css("text-align","center")
+					.css("font-family","hwxk")
+					.css("color",
+						((this.items[i].color == undefined)?
+							"#000"
+						:
+							this.items[i].color
+						)
+					)
+					.css("background-color",
+						((this.items[i].background_color == undefined)?
+							"#FFF"
+						:
+							this.items[i].background_color
+						)
+					)
+					.css("font-size",
+						((this.items[i].font_size == undefined)?
+							"30px"
+						:
+							this.items[i].font_size
+						)
+					)
+					.css("height",
+						((this.items[i].height == undefined)?
+							"50px"
+						:
+							this.items[i].height
+						)
+					)
+					.css("line-height",
+						((this.items[i].height == undefined)?
+							"50px"
+						:
+							this.items[i].height
+						)
+					)
+					.css("border",
+						((this.items[i].b_size == undefined)?
+							"1px"
+						:
+							this.items[i].size
+						)+
+						" solid "+
+						((this.items[i].b_color == undefined)?
+							"#AAA"
+						:
+							this.items[i].b_color
+						)
+					)
+					.click(this.items[i].onclick)
+			);
+		}
+	}
+	this.init();
+	this._items=function(value){
+		if (value == undefined){
+			return this.itmes;
+		}else{
+			this.items=value;
+			this.init();
+			return this;
+		}
+	};
+	this._box=function(value){
+		if (value == undefined){
+			return this.box;
+		}else{
+			this.box=value;
+			this.init();
+			return this;
+		}
+	};
+	this._random=function (){
+		var randomNumber=function(){
+			return 0.5-Math.random();
+		};
+		this.items.sort(randomNumber);
+		this.init();
+		return this;
+	};
+	this.show=function(){
+		this.box.hide();
+		$("body").append(this.box);
+		this.box.show("fast");
+		return this;
+	};
+	this.close=function(){
+		var box=this.box; //防止无法在函数内使用this
+		this.box.hide("fast",function(){
+			box.remove();
+		});
+		return this;
+	};
+}
--- server.js
+++ server.js
@@ -6,43 +6,28 @@ const fs = require("fs");
 var usocket = new Array;
 console.log(__dirname);
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + './index.html');
+app.get('*', function(req, res){
+	if (req.url == '/'){
+		res.sendFile(__dirname + '\\index.html');
+	}else{
+		res.sendFile(__dirname + '\\' + req.url);
+	}
 });
 
 io.on('connection', function(socket){
-	console.log('连接成功');
+	//console.log('连接成功');
 	
 	socket.on("disconnect",function(){
-		console.log("连接中断");
+		//console.log("连接中断");
 	});
 	
-	socket.on("sign_in",function(name,password){ //登录
-		var user=fs.readFileSync('./data/user.txt', 'UTF-8').split('\n');
-		for (var i in user){
-			if (user[i].split(' ')[0] == name & user[i].split(' ')[0] == password){
-				io.emit('sign_in',true);
-				return;
-			}
-		}
-		io.emit('sign',false);
-	});
-	socket.on("sign_up",function(name,password){ //注册
-		var user=fs.readFileSync('./data/user.txt', 'UTF-8').split('\n');
-		for (var i in user){
-			if (user[i].split(' ')[0] == name){
-				io.emit('sign_up',false);
-				return;
-			}
-		}
-		user.push(name+' '+password);
-		fs.writeFileSync('./data/user.txt', user.join('\n'), (err) => {
-			io.emit('sign_up',err);
-		});
-		io.emit('sign_up',true);
+	socket.on("test",function(id){
+		console.log('test:'+id);
+		io.emit("test",id);
 	});
 });
 
 http.listen(6060, "192.168.31.89", function() {
 	console.log('listening on 192.168.31.89:6060');
 });
+
