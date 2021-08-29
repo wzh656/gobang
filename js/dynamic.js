6e7b0faf6712fb8cb171bcc279dcd10d816f9c4d
wzh656 <1826632591@qq.com>
2021-08-29 22:37:51 +0800

    -stats 动态js

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,5 +1,10 @@
-if (location.pathname.indexOf("index.html") != -1){ //仅主页
+if (sessionStorage.getItem("version_update_showed") != 1){ //未显示
 	const updateInfo = {
+		14: {
+			version: "v4.4.0",
+			date: "2021.8.29",
+			info: "解决了很狗的特性：凉凉一直播放到下一局<br>增加了游戏结束字体显示和AI聊天功能<br><br>TapTap上可能暂未更新，因为员工周末休假去了不审核（竟然星期五下午就休假），要星期一才会回来，故想提前更新的可以去蓝奏云"
+		},
 		13: {
 			version: "v4.3.2",
 			date: "2021.8.27",
@@ -13,7 +18,7 @@ if (location.pathname.indexOf("index.html") != -1){ //仅主页
 			for (const [code, value] of Object.entries(updateInfo))
 				if (+code > plus.runtime.versionCode)
 					text += `==================<br>v${value.version}(${code}) ${value.date}<br>${value.info}`;
-			layer.confirm(`v${plus.runtime.version}(${plus.runtime.versionCode})->v${updateInfo[latest].version}(${latest})<br>${text}`, {
+			layer.confirm(`v${plus.runtime.version}(${plus.runtime.versionCode})->${updateInfo[latest].version}(${latest})<br>${text}`, {
 				title:"检测到新版本",
 				btn: ["更新（密码wzh）","取消"]
 			}, function(index){
@@ -21,6 +26,7 @@ if (location.pathname.indexOf("index.html") != -1){ //仅主页
 				layer.close(index);
 			});
 		}
+		sessionStorage.setItem("version_update_showed", 1);
 	}
 	if (typeof plus != "undefined"){
 		updateFunc();
