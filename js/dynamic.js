88751d0699b4b9d95fc8edf4d2b0819d11d89ab2
wzh <60345146+wzh656@users.noreply.github.com>
2021-10-02 21:39:19 +0800

    Update dynamic.js

--- js/dynamic.js
+++ js/dynamic.js
@@ -2,7 +2,7 @@ if (+new Date() >= +new Date(2021,9,1) && +new Date() <= +new Date(2021,9,7)){
 	$("body").css("background-image", "url('https://img2.baidu.com/it/u=771782317,2119761893&fm=26&fmt=auto')");
 	$("body > h1").append($("<small>国庆节快乐！</small>").css("font-size", "0.3em"));
 	if (localStorage.getItem("国庆节礼包弹窗-2021-10-01") != 1){
-		layer.alert("热烈庆祝我伟大的祖国72岁生日快乐！顺便祝我自己、同学游文庆15岁生日快乐！再预祝同学邱王顺6号生日快乐！（貌似这活动消息晚发了一天，不管了，就当是10月1日发的吧）<br>过节当然得发礼包，就搞一个图片当国庆节礼包吧（别说寒酸，真没啥好当礼包的了）<img src='https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0420%2F989aaf58j00qrueh8000mc000dw00b7g.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1635754172&t=51f92e9f7712d7e9294a4ec2363abe7f' style='width: 100%;' /><br/><iframe src='https://wzh656.github.io/others/some/gift/index.html' style='width: 100%;' ></iframe>", {title: "国庆节快乐！"});
+		layer.alert("热烈庆祝我伟大的祖国72岁生日快乐！顺便祝我自己、同学游文庆15岁生日快乐！再预祝同学邱王顺6号生日快乐！（貌似这活动消息晚发了一天，不管了，就当是10月1日发的吧）<br>非常感谢大家对游戏的支持与鼓励，我本以为这游戏凉了的，感谢大家用好评重新刷起热度，这是我收到过最好的生日礼物。<br>过节当然得发礼包，就搞一个图片和一个动画当国庆节礼包吧（别说寒酸，真没啥好当礼包的了）<img src='https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0420%2F989aaf58j00qrueh8000mc000dw00b7g.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1635754172&t=51f92e9f7712d7e9294a4ec2363abe7f' style='width: 100%;' /><br/><iframe src='https://wzh656.github.io/others/some/gift/index.html' style='width: 100%;' ></iframe>", {title: "国庆节快乐！"});
 		localStorage.setItem("国庆节礼包弹窗-2021-10-01", 1);
 	}
 }
